7693c063f168bfc324bafc9c3a6d3adf26967559
suraj <surajdey8603@gmail.com>
2020-06-03 21:36:24 +0530

    Dp Problem has been added
    Signed-off-by: suraj <surajdey8603@gmail.com>

---
+++ LinkedList/src/com/dp/lps/LongestPalindromicSequence.java
@@ -0,0 +1,49 @@
+package com.dp.lps;
+
+import com.dp.lcs.LongestCommonSubsequence;
+
+public class LongestPalindromicSequence {
+    public int  lcs(String s1)
+    {
+        StringBuilder s2=new StringBuilder();
+        for(int i=s1.length()-1;i>=0;i--)
+        {
+            s2.append(s1.charAt(i));
+        }
+        s2.toString();
+        int dp[][]=new int[s1.length()+1][s2.length()+1];
+        for(int i=0;i<dp.length;i++)
+        {
+            for(int j=0;j<dp[0].length;j++)
+            {
+                if(i==0 || j==0)
+                {
+                    dp[i][j]=0;
+
+                }
+            }
+        }
+        for(int i=1;i<dp.length;i++)
+        {
+            for(int j=1;j<dp[0].length;j++)
+            {
+                if(s1.charAt(i-1)==s2.charAt(j-1))
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+
+                }
+                else
+                {
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }
+
+    public static void main(String[] args) {
+        LongestPalindromicSequence longestPalindromicSequence= new LongestPalindromicSequence();
+        int result=longestPalindromicSequence.lcs("aba");
+        System.out.println(result);
+    }
+}
