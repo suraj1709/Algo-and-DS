1b1b5052a3041be6ff83d80a2589cfb6666070f9
suraj <surajdey8603@gmail.com>
2020-06-03 13:15:15 +0530

    Dp Problem has been added
    Signed-off-by: suraj <surajdey8603@gmail.com>

--- LinkedList/src/com/dp/lcs/PrintLcs.java
+++ LinkedList/src/com/dp/lcs/PrintLcs.java
@@ -1,6 +1,6 @@
 package com.dp.lcs;
 
-public class LongestCommonSubstring {
+public class PrintLcs {
     /*
      * Given two String find the longest sequence between them:
      * S1="abcdf"
@@ -8,7 +8,7 @@ public class LongestCommonSubstring {
      * Result=>3
      * */
 
-    public int  lcs(String s1,String s2)
+    public String  lcs(String s1,String s2)
     {
         int dp[][]=new int[s1.length()+1][s2.length()+1];
         for(int i=0;i<dp.length;i++)
@@ -33,24 +33,40 @@ public class LongestCommonSubstring {
                 }
                 else
                 {
-                    dp[i][j]=0;
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-        int maxSubstringLength=0;
-        for(int i=0;i<dp.length;i++)
+
+        int i=dp.length-1;
+        int j=dp[0].length-1;
+        StringBuilder stringBuilder=new StringBuilder();
+        while(i>0 && j>0)
         {
-            for(int j=0;j<dp[0].length;j++)
+            if(s1.charAt(i-1)==s2.charAt(j-1))
             {
-                maxSubstringLength=Math.max(maxSubstringLength,dp[i][j]);
+                stringBuilder.append(s1.charAt(i-1));
+                i--;
+                j--;
+
+            }
+            else {
+                if(dp[i-1][j]>dp[i][j-1])
+                {
+                    i--;
+                }
+                else
+                {
+                    j--;
+                }
             }
         }
-        return maxSubstringLength;
+        return stringBuilder.toString();
     }
 
     public static void main(String[] args) {
-        LongestCommonSubstring longestCommonSubstring= new LongestCommonSubstring();
-        int result=longestCommonSubstring.lcs("abc","abc");
+        PrintLcs printLcs= new PrintLcs();
+        String result=printLcs.lcs("abcdef","bcfg");
         System.out.println(result);
     }
 }
